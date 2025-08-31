e0f29fd4db408e10652b6635c0d896c7e8fb0797
Patrick Styll <styllpatrick@gmail.com>
2025-08-31 22:06:30 +0000

    Implemented Baum-Welch Algorithm

--- dummer-build.cc
+++ dummer-build.cc
@@ -1,4 +1,5 @@
 // Author: Martin C. Frith 2025
+//         Patrick Styll   2025
 
 #include "dummer-util.hh"
 
@@ -22,10 +23,14 @@
 #include <getopt.h>
 
 // Copied from HMMER 3.4:
-#define OPT_symfrac 0.5
-#define OPT_ere_aa 0.59
-#define OPT_ere_nt 0.62
-#define OPT_esigma 45.0
+#define OPT_symfrac 0.50
+#define OPT_ere_aa  0.59
+#define OPT_ere_nt  0.62
+#define OPT_esigma  45.0
+
+// Baum Welch parameters:
+#define OPT_bw_maxiter 1000
+#define OPT_bw_maxDiff 1e-6
 
 int verbosity = 0;
 
@@ -122,15 +127,14 @@ void applyDirichletMixture(DirichletMixture dmix,
 void gapCountsToProbs(const GapPriors &gp, double maxCountSum,
 		      const double *counts,
 		      double matchPrev, double alnBegNext, double *probs) {
-  double alnBeg = counts[0];
-  double match  = counts[1];
-  double delBeg = counts[2];
-  double insBeg = counts[3];
-  double insEnd = insBeg;
-  double insExt = counts[4];
-  double delEnd = counts[5];
-  double delExt = counts[6];
-  double alnEnd = alnBeg + matchPrev - match + delEnd - delBeg;
+  double alnBeg = counts[0]; // etap
+  double match  = counts[1]; //gamma
+  double delBeg = counts[2]; //delta
+  double insBeg = counts[3]; //alpha
+  double insEnd = insBeg;    //betap
+  double insExt = counts[4]; //beta
+  double delEnd = counts[5]; //epsilonp
+  double delExt = counts[6]; //epsilon
 
   // The "- alnBeg" avoids the insertion start probability changing
   // if the aligned sequences are reversed:
@@ -143,20 +147,6 @@ void gapCountsToProbs(const GapPriors &gp, double maxCountSum,
   double b = getProb(insExt, insEnd, gp.insExtend, gp.insEnd, maxCountSum);
   probs[3] = 1 - b;
   probs[4] = b;  // insertion extend probability
-
-  // This aims to stop the deletion probability being too low around
-  // starts & ends of sequences.  For example, the deletion count in
-  // the profile's first and last position is always zero (because end
-  // gaps aren't counted).  Pretend there's a deletion at both edges
-  // of each sequence (in other words, count one end gap each side as
-  // a deletion):
-  delBeg += alnEnd + alnBegNext;
-
-  // Likewise, this aims to refine the deletion extend probability
-  // around starts of sequences.  (What about ends of sequences?  Too
-  // much for my poor brain...)
-  delEnd += alnBeg;      // stop the deletion extend probability being too high
-  delExt += alnBegNext;  // stop the deletion extend probability being too low
 
   double d = getProb(delBeg, match, gp.delStart, gp.match, maxCountSum);
   probs[0] = (1 - a) * (1 - d);
@@ -287,8 +277,8 @@ std::istream &readMultipleAlignment(std::istream &in, MultipleAlignment &ma) {
   unsigned char *a = ma.alignment.data();
 
   for (int i = 0; i < ma.sequenceCount; ++i) {
-    for (int j = 0; j < lines.size(); j += ma.sequenceCount) {
-      for (int k = 0; k < lines[i + j].size(); ++k) {
+    for (size_t j = 0; j < lines.size(); j += ma.sequenceCount) {
+      for (size_t k = 0; k < lines[i + j].size(); ++k) {
 	unsigned char c = lines[i + j][k];
 	*a++ = c;
       }
@@ -443,7 +433,314 @@ void countEvents(const MultipleAlignment &ma, int alphabetSize, double symfrac,
   // We'll estimate probabilities from these counts, but there are 2 problems:
   // 1. Adjacent inserts (or deletes) are assumed to be extension, not restart.
   // 2. The counts assume the alignment is exactly correct and certain.
-  // We could mitigate both problems by using a Baum-Welch algorithm.
+  // We will mitigate both problems by using a Baum-Welch algorithm.
+}
+
+void getSequenceWithoutGaps(
+     const MultipleAlignment &ma, int alphabetSize,
+     std::vector<unsigned char> &seqNoGap,
+     std::vector<unsigned int> &seqLengths) {
+  for (int i = 0; i < ma.sequenceCount; i++) {
+    const unsigned char *seq = ma.alignment.data() + i * ma.alignmentLength;
+    for (int j = 0; j < ma.alignmentLength; j++) {
+      if (seq[j] <= alphabetSize) {
+        seqNoGap.push_back(seq[j]);
+        seqLengths[i]++;
+      }
+    }
+  }
+}
+
+void forward(
+     unsigned char *seq, int seqLength, std::vector<double> &probs,
+     int profileLength, int width, double *wSum,
+     std::vector<double> &X, std::vector<double> &Y, std::vector<double> &Z) {
+
+  double aPrime, bPrime, dPrime, ePrime;  // parameters for BW
+  double alphaProb, betaProb, deltaProb, epsilonProb, epsilonProb1;
+
+  for (int i = 1; i <= profileLength+1; i++) {
+
+    alphaProb    = probs[(i-1) * width + 1];  // insStart
+    betaProb     = probs[(i-1) * width + 4];  // insExt
+    deltaProb    = probs[(i-1) * width + 2];  // delStart
+    epsilonProb  = probs[(i-1) * width + 6];  // delExt
+
+    epsilonProb1 = (i == profileLength+1) ? 0.0 : probs[i * width + 6];
+
+    aPrime = alphaProb * (1.0 - betaProb);
+    bPrime = betaProb;
+
+    // can use arbitrary values for S_m, d_m, e_m
+    dPrime = deltaProb   * (1 - epsilonProb1);
+    ePrime = epsilonProb * (1 - epsilonProb1) / (1 - epsilonProb);
+    if (!std::isfinite(ePrime)) ePrime = 0.0; // avoid NaN issues
+
+    for (int j = 1; j <= seqLength+1; j++) {
+
+      int letter = j == seqLength+1 ? 0 : seq[j-1];
+
+      // letter probs / background probs
+      double S = (1 - alphaProb - deltaProb)
+               * probs[(i-1) * width + (7 + letter)]
+               / probs[profileLength * width + (7 + letter)];
+      if (!std::isfinite(S)) S = 0.0; // if letter has 0 background probability
+
+      double w = X[(i-1) * (seqLength+2) + (j-1)]
+               + Y[(i-1) * (seqLength+2) + j]
+               + Z[i * (seqLength+2) + (j-1)] + 1.0;
+      *wSum += w;
+
+      X[i * (seqLength+2) + j] = S * w;
+      Y[i * (seqLength+2) + j] = dPrime * w
+                               + ePrime * Y[(i-1) * (seqLength+2) + j];
+      Z[i * (seqLength+2) + j] = aPrime * w
+                               + bPrime * Z[i * (seqLength+2) + (j-1)];
+    }
+  }
+}
+
+void backward(unsigned char *seq, int seqLength,
+     std::vector<double> &probs, int profileLength, int width,
+     std::vector<double> &Wbar, std::vector<double> &Ybar, std::vector<double> &Zbar) {
+
+  double aPrime, bPrime, dPrime, ePrime;  // parameters for BW
+  double alphaProb, betaProb, deltaProb, epsilonProb, epsilonProb1;
+
+  for (int i = profileLength; i >= 0; i--) {
+
+    alphaProb    = probs[i * width + 1];  // insStart
+    betaProb     = probs[i * width + 4];  // insExt
+    deltaProb    = probs[i * width + 2];  // delStart
+    epsilonProb  = probs[i * width + 6];  // delExt
+
+    epsilonProb1 = (i == profileLength) ? 0.0 : probs[(i+1) * width + 6];
+
+    aPrime = alphaProb * (1.0 - betaProb);
+    bPrime = betaProb;
+
+    // can use arbitrary values for S_m, d_m, e_m
+    dPrime = deltaProb   * (1 - epsilonProb1);
+    ePrime = epsilonProb * (1 - epsilonProb1) / (1 - epsilonProb);
+    if (!std::isfinite(ePrime)) ePrime = 0.0; // avoid NaN issues
+
+    for (int j = seqLength; j >= 0; j--) {
+
+      int letter = j == seqLength ? 0 : seq[j];
+
+      // letter probs / background probs
+      double S = (1 - alphaProb - deltaProb)
+               * probs[i * width + (7 + letter)]
+               / probs[profileLength * width + (7 + letter)];
+      if (!std::isfinite(S)) S = 0.0; // if letter has 0 background probability
+
+      double x = S * Wbar[(i+1) * (seqLength+2) + (j+1)];
+
+      Wbar[i * (seqLength+2) + j] = x + dPrime * Ybar[(i+1) * (seqLength+2)+ j]
+                                  + aPrime * Zbar[i * (seqLength+2) + (j+1)]
+                                  + 1.0; // score is 1.0
+      Ybar[i * (seqLength+2) + j] = Wbar[i * (seqLength+2) + j]
+                                  + ePrime * Ybar[(i+1) * (seqLength+2) + j];
+      Zbar[i * (seqLength+2) + j] = Wbar[i * (seqLength+2) + j]
+                                  + bPrime * Zbar[i * (seqLength+2) + (j+1)];
+    }
+  }
+}
+
+int determineTerminationCondition(double bwMaxDiff,
+       std::vector<double> &probsOld, std::vector<double> &probsNew) {
+  double maxDiff = 0.0;
+  for (size_t i = 0; i < probsOld.size(); i++) {
+    double diff = fabs(probsOld[i] - probsNew[i]);
+    if (diff > maxDiff) maxDiff = diff;
+  }
+  return (maxDiff <= bwMaxDiff) ? 1 : 0; // converged or not
+}
+
+void calculateTransitionCounts(
+     std::vector<double> &counts, int profileLength, int seqLength,
+     std::vector<double> &Wbar, std::vector<double> &Ybar,
+     std::vector<double> &Zbar, std::vector<double> &X,
+     std::vector<double> &Y,    std::vector<double> &Z,
+     double wt, int width, int alphabetSize, unsigned char *seq) {
+
+  double alpha, beta, delta, epsilon;
+  double gamma, betap, epsilonp, etap;
+  double epsilonpI1, epsilonI1;
+
+  // estimate counts for every position
+  for (int i = 1; i <= profileLength+1; i++) {
+
+    betap    = 0.0;  // expected count of (1 - beta)
+    beta     = 0.0;
+    epsilonp = 0.0;  // expected count of (1 - epsilon)
+    epsilon  = 0.0;
+    etap     = 0.0;  // expected count of (1 - eta)
+
+    for (int j = 1; j <= seqLength+1; j++) {
+      betap    += Z[i * (seqLength+2) + (j-1)]
+                * Wbar[(i-1) * (seqLength+2) + (j-1)];
+      beta     += Z[i * (seqLength+2) + (j-1)]
+                * Zbar[(i-1) * (seqLength+2) + (j-1)];
+      epsilonp += Y[(i-1) * (seqLength+2) + j]
+                * Wbar[(i-1) * (seqLength+2) + (j-1)];
+      epsilon  += Y[(i-1) * (seqLength+2) + j]
+                * Ybar[(i-1) * (seqLength+2) + (j-1)];
+      etap     += Wbar[(i-1) * (seqLength+2) + (j-1)];
+    }
+
+    beta  -= betap;
+    if (beta < -__DBL_EPSILON__)
+      std::cerr << "Warning: Negative beta count at position " << i << "\n";
+
+    epsilon  -= epsilonp;
+    if (epsilon < -__DBL_EPSILON__)
+      std::cerr << "Warning: Negative epsilon count at position " << i << "\n";
+
+    // expected count of alpha
+    alpha = betap;
+
+    // expected count of gamma
+    // gamma_m is arbitrary
+    gamma = 0.0;
+
+    // expected count of delta
+    delta = 0.0;
+    // requires epsilonp_i+1 and epsilon_i+1
+
+    // epsilon_m may be arbitrary, therefore 0 is fine
+    if (i <= profileLength) {
+      epsilonpI1 = 0.0;
+      epsilonI1  = 0.0;
+      for (int j = 1; j <= seqLength+1; j++) {
+        epsilonpI1 += Y[i * (seqLength+2) + j]
+                    * Wbar[i * (seqLength+2) + (j-1)]; // i++
+        epsilonI1  += Y[i * (seqLength+2) + j]
+                    * Ybar[i * (seqLength+2) + (j-1)]; // i++
+      }
+      epsilonI1  -= epsilonpI1;
+
+      // now we can calculate delta
+      delta = epsilonpI1 + epsilonI1 - epsilon;
+      if (delta < -__DBL_EPSILON__)
+        std::cerr << "Warning: Negative delta count at position " << i << "\n";
+      for (int j = 1; j <= seqLength; j++) {
+        gamma += X[i * (seqLength+2) + j] * Wbar[i * (seqLength+2) + j];
+      }
+    }
+
+    // update the HMM parameters
+    counts[(i-1) * width + 0] += etap     * wt;
+    counts[(i-1) * width + 1] += gamma    * wt;
+    counts[(i-1) * width + 2] += delta    * wt;
+    counts[(i-1) * width + 3] += alpha    * wt;
+    counts[(i-1) * width + 4] += beta     * wt;
+    counts[(i-1) * width + 5] += epsilonp * wt;
+    counts[(i-1) * width + 6] += epsilon  * wt;
+
+    // emission probabilities
+    if (i == profileLength+1) continue; // no emissions from the end state
+    for (int letter = 0; letter < alphabetSize; letter++) {
+      double emis = 0.0;
+      for (int j = 1; j <= seqLength; j++) {
+        if (seq[j-1] == letter)
+          emis += X[i * (seqLength+2) + j] * Wbar[i * (seqLength+2) + j];
+      }
+      counts[(i-1) * width + (7 + letter)] += emis * wt;
+    }
+  }
+}
+
+void baumWelch(std::vector<double> &counts, const MultipleAlignment &ma,
+     int alphabetSize, DirichletMixture dmix, const GapPriors &gp,
+     int profileLength, const double *weights,
+     double maxIter, double bwMaxDiff) {
+
+  // transitions + alphabet
+  int width = 7 + alphabetSize;
+  std::vector<double> probsOld(counts.size() + alphabetSize);
+  std::vector<double> probsNew(counts.size() + alphabetSize);
+
+  int rows = profileLength      + 2;
+  int cols = ma.alignmentLength + 2;
+
+  std::vector<double> Wbar(rows * cols, 0.0);
+  std::vector<double> Ybar(rows * cols, 0.0);
+  std::vector<double> Zbar(rows * cols, 0.0);
+  std::vector<double> X(rows * cols, 0.0);
+  std::vector<double> Y(rows * cols, 0.0);
+  std::vector<double> Z(rows * cols, 0.0);
+
+  /* Get gapless sequences and their lengths. */
+  std::vector<unsigned char> seqsNoGap;
+  std::vector<unsigned int>  seqsLengths(ma.sequenceCount, 0);
+  getSequenceWithoutGaps(ma, alphabetSize, seqsNoGap, seqsLengths);
+
+  countsToProbs(dmix, gp, alphabetSize, 1e37, profileLength,
+      counts.data(), probsOld.data());
+
+  int termCond;
+  int num_iterations = 0;
+
+  do {
+
+    /* We aggregate the expected counts over all sequences. */
+    for (size_t i = 0; i < counts.size(); i++) counts[i] = 0.0;
+    std::fill(probsNew.begin(), probsNew.end(), 0.0);
+
+    int seqs_total_length = 0;
+
+    for (int idx = 0; idx < ma.sequenceCount; idx++) {
+
+      // reset the forward and backward matrices
+      std::fill(Wbar.begin(), Wbar.end(), 0.0);
+      std::fill(Ybar.begin(), Ybar.end(), 0.0);
+      std::fill(Zbar.begin(), Zbar.end(), 0.0);
+      std::fill(X.begin(), X.end(), 0.0);
+      std::fill(Y.begin(), Y.end(), 0.0);
+      std::fill(Z.begin(), Z.end(), 0.0);
+
+      double wt = weights[idx]; // weight for the current sequence
+
+      unsigned char* seqNoGap = seqsNoGap.data() + seqs_total_length;
+
+      /* Forward pass, calculate X, Y, Z and
+         the aggregated v (i.e. sum of w-values). */
+      double v = 0.0; // accumulate the sum of weights
+      forward(seqNoGap, seqsLengths[idx], probsOld,
+        profileLength, width, &v, X, Y, Z);
+
+      /* Backward pass, calculate Wbar, Ybar, Zbar. */
+      backward(seqNoGap, seqsLengths[idx], probsOld,
+        profileLength, width, Wbar, Ybar, Zbar);
+
+      /* Calculate and update parameters in new parameter counts. */
+      calculateTransitionCounts(counts, profileLength, seqsLengths[idx],
+          Wbar, Ybar, Zbar, X, Y, Z, wt/v, width, alphabetSize, seqNoGap);
+
+      seqs_total_length += seqsLengths[idx];
+    }
+
+    /* Turn the parameter counts into probabilities via priors. */
+    countsToProbs(dmix, gp, alphabetSize, 1e37, profileLength,
+        counts.data(), probsNew.data());
+
+    /* Determine termination condition.
+       Then overwrite the old probabilities with the new ones. */
+    termCond = determineTerminationCondition(bwMaxDiff, probsOld, probsNew);
+
+    std::copy(probsNew.begin(), probsNew.end(), probsOld.begin());
+
+    std::cerr << "\rIteration " << ++num_iterations << " / " << maxIter << ": "
+              << (termCond ? "    converged" : "not converged");
+
+  } while (!termCond && num_iterations < maxIter);
+
+  std::cerr << "\nError tolerance: " << bwMaxDiff
+            << ", iterations: " << num_iterations << ", "
+            << (termCond ? "Baum-Welch converged" : "Baum-Welch not converged")
+            << "\n";
+
 }
 
 void printProb(double prob) {
@@ -499,8 +796,12 @@ void printProfile(const double *probs, const int *columns,
 
 int main(int argc, char* argv[]) {
   double symfrac = OPT_symfrac;
-  double ere = 0;
-  double esigma = OPT_esigma;
+  double ere     = 0;
+  double esigma  = OPT_esigma;
+
+  double bwMaxiter  = OPT_bw_maxiter;
+  double bwMaxDiff = OPT_bw_maxDiff;
+  bool   countOnly   = false;
 
   const char help[] = "\
 usage: dummer-build alignments.stk\n\
@@ -523,18 +824,28 @@ Options for effective sequence number:\n\
     STR(OPT_ere_nt) " for nucleotide)\n\
   --esigma E     aim for this relative entropy (default: "
     STR(OPT_esigma) ")\n\
-";
+  \n\
+Baum-Welch options:\n\
+  --maxiter N    maximum number of Baum-Welch iterations (default: "
+    STR(OPT_bw_maxiter) ")\n\
+  --maxdiff X    convergence threshold for Baum-Welch (default: "
+    STR(OPT_bw_maxDiff) ")\n\
+  --countonly    only count events, do not run Baum-Welch\n\
+  ";
 
   const char sOpts[] = "hVv";
 
   static struct option lOpts[] = {
-    {"help",    no_argument,       0, 'h'},
-    {"version", no_argument,       0, 'V'},
-    {"verbose", no_argument,       0, 'v'},
-    {"symfrac", required_argument, 0, 's'},
-    {"enone",   no_argument,       0, 'n'},
-    {"ere",     required_argument, 0, 'p'},
-    {"esigma",  required_argument, 0, 'e'},
+    {"help",      no_argument,       0, 'h'},
+    {"version",   no_argument,       0, 'V'},
+    {"verbose",   no_argument,       0, 'v'},
+    {"symfrac",   required_argument, 0, 's'},
+    {"enone",     no_argument,       0, 'n'},
+    {"ere",       required_argument, 0, 'p'},
+    {"esigma",    required_argument, 0, 'e'},
+    {"maxiter",   required_argument, 0, 'N'},
+    {"maxdiff",   required_argument, 0, 'X'},
+    {"countonly", no_argument,       0, 'c'},
     {0, 0, 0, 0}
   };
 
@@ -566,6 +877,17 @@ Options for effective sequence number:\n\
     case 'e':
       esigma = strtod(optarg, 0);
       if (esigma <= 0) return badOpt();
+      break;
+    case 'N':
+      bwMaxiter = strtol(optarg, 0, 0);
+      if (bwMaxiter <= 0) return badOpt();
+      break;
+    case 'X':
+      bwMaxDiff = strtod(optarg, 0);
+      if (bwMaxDiff < 0) return badOpt();
+      break;
+    case 'c':
+      countOnly = true;
       break;
     case '?':
       std::cerr << help;
@@ -632,6 +954,11 @@ Options for effective sequence number:\n\
     countEvents(ma, alphabetSize, symfrac, weights.data(), weightSum,
 		counts, columns);
     int profileLength = columns.size();
+
+    if (!countOnly) {
+      baumWelch(counts, ma, alphabetSize, dmix, gp,
+          profileLength, weights.data(), bwMaxiter, bwMaxDiff);
+    }
 
     // the "+ alphabetSize" is space for background letter probabilities:
     std::vector<double> probs(counts.size() + alphabetSize);
