ccc334d82df7452edb99176a5bbe6f267ed5ff49
Harvey Chua <harvs89.2@gmail.com>
2026-06-11 18:18:26 +0800

    Use bin directory again

--- test.sh
+++ test.sh
@@ -1,5 +1,5 @@
 #time bash bin/pipeline.sh AMRFinder_filtered.hmm originals300.fasta
 #time bash bin/pipeline.sh AMRFinder_filtered.hmm originals300.fasta
-time build/dummer -v MET-test.hmm met-optimized.fa
+time bin/dummer -v MET-test.hmm met-optimized.fa
 #time bash bin/pipeline.sh MET-test.hmm MET-target.fa
 #time bin/dummer -v /home/harvsftw/Documents/bioinfo/BATH-paper/transmark/test-001.AA.hmm /mnt/tmp/test.txt
