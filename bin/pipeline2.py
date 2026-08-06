2064dcfc39779e76e3c804c01ea863ea839120b6
Harvey Chua <harvs89.2@gmail.com>
2026-08-06 17:04:23 +0800

    Make forward-only filter stricter

--- bin/pipeline2.py
+++ bin/pipeline2.py
@@ -373,7 +373,7 @@ def main():
             #custom_env["ASAN_OPTIONS"] = "detect_container_overflow=1:strict_memcmp=1"
             
             try:
-                subprocess.run([dummer_exec, hmm_file, merged_fa_path, '-T 8', '-W 0.5' if not args.max else '-W 10', '-N', str(tot_seq_len)], env=custom_env, check=True)
+                subprocess.run([dummer_exec, hmm_file, merged_fa_path, '-T 8', '-W 0.001' if not args.max else '-W 10', '-N', str(tot_seq_len)], env=custom_env, check=True)
             except subprocess.CalledProcessError as e:
                 print(f"Error: dummer encountered an issue (Exit status: {e.returncode})")
                 sys.exit(1)
